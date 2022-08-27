1a8e64fe7894acfd58baade45abe82914af176ae
Hangsihak Sin <sihak.hs@gmail.com>
2022-08-27 03:42:39 -0400

    Fixing delpoyment issue by reinstalling package.json

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,5 @@
-import React from 'react';
 import Job from '../Job';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Image,
-  Container,
-  Flex,
-} from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 
 const Layout = () => {
   return (
